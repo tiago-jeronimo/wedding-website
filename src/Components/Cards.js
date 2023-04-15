a3363cf0bdba78991811d2761cf97f367fe6d6f7
Tiago Jerônimo <tiago.batmansz@gmail.com>
2023-04-14 23:29:24 -0300

    Cria storageFirebase, renderiza informações dinâmicas por um falso bd

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,6 @@
 import React from "react";
-import qr_code from "./qr_code.png";
+import presentes from "../data/listPresentes";
+// import qr_code from "./qr_code.png";
 import "./Card.css";
 
 class Cards extends React.Component {
@@ -9,26 +10,28 @@ class Cards extends React.Component {
         <div class="flip-card">
           <div class="flip-card-inner">
             <div class="flip-card-front">
-              <img
-                id="imagem-card"
-                src="https://a-static.mlcdn.com.br/150x150/ferro-de-passar-roupa-a-vapor-e-a-seco-arno-steamgliss-verde/magazineluiza/021958200/fc091e5cbbb557059c89cf18e9f6bb7a.jpg"
-                alt="Avatar"
-              />
-              <div className="texto__front card01" >
-                <p className="texto__front card02" >QUERO PRESENTEAR COM </p>
-                <div className="div__product" >
-                    <h2 className="texto__front" >Ferro de Passar</h2>
-                    <p className="texto__front" >R$100</p>
+              <img id="imagem-card" src={presentes[0].image} alt="Avatar" />
+              <div className="texto__front card01">
+                <p className="texto__front card02">QUERO PRESENTEAR COM </p>
+                <div className="div__product">
+                  <h2 className="texto__front">{presentes[0].product}</h2>
+                  <p className="texto__front">R${presentes[0].price},00</p>
                 </div>
-                <div className="div__click" >
-                  <p className="texto__front texto__click" >Click no card para acesso ao QRcode</p>
+                <div className="div__click">
+                  <p className="texto__front texto__click">
+                    Click no card para acesso ao QRcode
+                  </p>
                 </div>
               </div>
             </div>
             <div class="flip-card-back">
-              <img id="imagem__back" alt="imagem de um produto" src={qr_code} />
-              <h1> Ferro de Passar</h1>
-              <p>R$100,00</p>
+              <img
+                id="imagem__back"
+                alt="imagem de um produto"
+                src={presentes[0].pix}
+              />
+              <h1> {presentes[0].product}</h1>
+              <p>R${presentes[0].price},00</p>
             </div>
           </div>
         </div>
---
+++ src/data/listPresentes.js
@@ -0,0 +1,17 @@
+const presentes = [
+    {
+        'id':'01',
+        'image':'https://images.tcdn.com.br/img/img_prod/1048952/180_ferro_de_passar_roupa_vapor_electrolux_siv10_perfect_glide_220v_307_1_83306e16e65821dbdb70d5e4bfdb8998_20210831035821.jpg',
+        'product':'Ferro de Passar',
+        'price':'100',
+        'pix':'https://firebasestorage.googleapis.com/v0/b/debora-e-tiago.appspot.com/o/cem.png?alt=media&token=96403c1e-f58a-4ca2-9e5b-52018e15b7d5',
+    },
+    {
+        'id':'01',
+        'product':'Ferro de Passar',
+        'price':'100',
+        'image':'qr_code',
+    }
+];
+
+export default presentes;
